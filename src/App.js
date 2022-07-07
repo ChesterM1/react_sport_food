2ac8f3eab7a1e3fa600b6b0fcef5ab4c60a8dc0d
AlexandrM <chestermen30@gmail.com>
2022-07-07 18:42:12 +0300

    order page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter,  Routes, Route} from 'react-router-dom';
+import  store  from './store/store';
+import { Provider } from 'react-redux';
+import Home from './components/pages/Home';
+import Card from './components/Card/Card';
+import ItemPage from './components/pages/Item_page';
+import OrderForm from './components/Card/Order_form/Order_form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+  return(
+    <>
+    <Provider store={store}>
+      <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Home/>}/>
+        <Route path="card" element={<Card/>}/>
+        <Route path={'/item/:itemId'} element={<ItemPage/>}/>
+        <Route path='card/order' element={<OrderForm/>}/>
+          
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+    
+    
+    </>
+   
+  )
 }
 
 export default App;
